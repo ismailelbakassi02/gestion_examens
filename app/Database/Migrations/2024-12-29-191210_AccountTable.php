3de81db2201d640570bc01b3417ca9c365949cc0
hamza <hamza.elmansouri.e@gmail.com>
2024-12-29 22:56:58 +0100

    Add migrations files Role,User,Account

---
+++ app/Database/Migrations/2024-12-29-191210_AccountTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AccountTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_account' => [
+                'type'           => 'INT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'id_user' => [
+                'type'       => 'INT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'null'       => false,
+            ],
+            'id_role' => [
+                'type'       => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'null'       => false,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+                'null'       => true,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+                'null'       => true,
+            ],
+        ]);
+        $this->forge->addKey('id_account', true);
+        $this->forge->addKey('id_user');
+        $this->forge->addKey('id_role');
+        $this->forge->addForeignKey('id_user', 'user', 'id_user', 'CASCADE', 'CASCADE'); 
+        $this->forge->addForeignKey('id_role', 'role', 'id_role', 'CASCADE', 'CASCADE'); 
+        $this->forge->createTable('account');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('account');
+    }
+}
